0125fd949bf8646ac7a42af08f42b9029a13d837
Lauris <laurisdid@gmail.com>
2022-03-29 13:14:20 +0300

    jsinfo

---
+++ js/variables.js
@@ -0,0 +1,34 @@
+
+
+
+
+const skaicius =123;
+const vardas='Petaras';
+const smallSkaicius=skaicius-1;
+console.log('vardas', vardas);
+console.log('skaicius -1',smallSkaicius);
+console.log('laba diena ', vardas ,'.');
+
+const pasisveikinimas ='laba diena '+ vardas +'.';
+
+/*
+KINTAMIEJI
+Iniciavimo budas:
+const - default iniciavimo budas
+let - antrinis budas inicijuoti, kai zinome, jog reiksme tures keistis
+var - pats seniausias ir geriau niekada nenaudoti, jei rupi saugumas
+Duomenu tipas: 
+- number (skaicius)
+- string (tekstas)
+- boolean (logine reiksme: true | false)
+- array (masyvas, sarasas, matrica*)
+*/
+
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('Pazymiai:', pazymiai);
+
+const studentai = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+console.log(studentai);
+
+const booleanList = [true, false, true, false, true, false];
+console.log(booleanList);
