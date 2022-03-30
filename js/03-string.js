d77dbdcad8b862183a91d67a9b3cecc1d751e3d5
Lauris <laurisdid@gmail.com>
2022-03-30 11:46:38 +0300

    +paskaitos darbas

---
+++ js/03-string.js
@@ -0,0 +1,29 @@
+/*
+String 
+
+kabutes:
+- viengubos(')
+-dvigubos(")
+*/
+const name ='Petaras';
+console.log(name);
+
+const lastname ="Petaraitis"
+console.log(lastname);
+
+const vienaKabute ="viengubos(')";
+console.log(vienaKabute)
+
+const dvigubosKabutes ='dvigubos(")';
+console.log(dvigubosKabutes)
+
+const viengubaKabute12 = 'cia yra vienguba (\')ir dviguba(")kabutes.';
+console.log(viengubaKabute12);
+
+const kabute12 = "cia yra vienguba (')ir dviguba(\")kabutes.";
+console.log(kabute12);
+
+const clientname= 'Maryte';
+const clientage= 77;
+const clientSentence = 'Client '+ clientname + ' is '+ clientage +' years old.';
+console.log(clientSentence);
