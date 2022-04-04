e428d7ebc6adeec2eec66d4d6cee0b33a15069e5
Lauris <laurisdid@gmail.com>
2022-04-04 12:29:09 +0300

    liekana js

---
+++ js/11-liekana.js
@@ -0,0 +1,6 @@
+function liekana(nuo, iki, daliklis){
+
+    return` skaiciu intervalas nuo  ${nuo} iki ${iki}, besidalijanciu be liekanos is ${daliklis} yra vienetai`
+}
+
+console.log(liekana(0,11,3))
